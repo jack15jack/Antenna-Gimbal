9a44427828e6ceb703b32ebf449fa04f8f2ee6b0
jack15jack <105609905+jack15jack@users.noreply.github.com>
2026-03-12 15:14:12 -0500

    Update bistatic.cpp

--- Gimbal Code/main/bistatic.cpp
+++ Gimbal Code/main/bistatic.cpp
@@ -7,7 +7,7 @@
 #include <SD.h>
 static unsigned long lastUpdateTime = 0;
 void handleBistaticState() {
-  static const unsigned long MOVE_INTERVAL_MS = 100;  // 1 second
+  static const unsigned long MOVE_INTERVAL_MS = 100; // 1 second
   float target_absolute_elevation = SEA_LEVEL_REFERENCE_M + TARGET_RELATIVE_TO_SEA_M;
   float vertical_offset = gnss_alt - target_absolute_elevation;
 
@@ -18,7 +18,7 @@ void handleBistaticState() {
   pitch_input = target_pitch;
   roll_input = target_roll;
 
-  // debug
+   // debug
   Serial.print("Target Pitch: ");
   Serial.print(target_pitch, 2);
   Serial.print(" | Target Roll: ");
@@ -35,27 +35,30 @@ void handleBistaticState() {
   Serial.println(gnss_lon, 2);
   Serial.println(TARGET_LAT, 2);
   Serial.println(TARGET_LON, 2);
-
+  
   pitch_input = (90 - target_pitch + roll_A);
   roll_input = pitch_A - target_roll;
 
   //int pitch_cmd = 90 - pitch_input;
   int roll_err = target_pitch + (roll_A - 90);
-  int pitch_cmd = 180 - target_pitch - roll_err;  //- roll_A;
-  int roll_cmd = 90 - roll_input;
-  if (millis() - lastUpdateTime >= MOVE_INTERVAL_MS) {
-    lastUpdateTime = millis();
-    roll_servo.write(pitch_cmd);
-    pitch_servo.write(roll_cmd);
-  }
+ // int pitch_cmd =   - target_pitch - roll_err;//- roll_A;
+  int pitch_cmd =    90- roll_err;//- roll_A;
+  int roll_cmd  = 90 - roll_input;
+ 
+if (millis() - lastUpdateTime >= MOVE_INTERVAL_MS) {
+lastUpdateTime = millis();
+  roll_servo.write(pitch_cmd);
+  pitch_servo.write(roll_cmd);
+}
   Serial.print("Pitch Servo: ");
   Serial.print(pitch_cmd);
   Serial.print(" | Roll Servo: ");
   Serial.println(roll_cmd);
-  Serial.print(" | Distance: ");
+Serial.print(" | Distance: ");
   Serial.println(distance);
   Serial.print(" | roll: ");
   Serial.println(roll_A);
-
+  
   writeToSDCard();
+
 }
